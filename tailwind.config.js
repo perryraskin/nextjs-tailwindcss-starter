91f14840c4778225665fb481858869f3d56b9d1e
Perry Raskin <perryraskin@gmail.com>
2021-04-12 21:03:48 -0400

    Upgrade TailwindCSS and clean up code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  mode: 'jit',
+  darkMode: 'media',
   purge: [
     "./pages/**/*.tsx",
     "./pages/**/*.jsx",
@@ -7,33 +9,11 @@ module.exports = {
     "./public/index.html",
     "./styles/tailwind.css"
   ],
-  theme: {
-    spinner: theme => ({
-      default: {
-        color: "#ffffff", // color you want to make the spinner
-        size: "1em", // size of the spinner (used for both width and height)
-        border: "2px", // border-width of the spinner (shouldn't be bigger than half the spinner's size)
-        speed: "500ms" // the speed at which the spinner should rotate
-      }
-      // md: {
-      //   color: theme('colors.red.500', 'red'),
-      //   size: '2em',
-      //   border: '2px',
-      //   speed: '500ms',
-      // },
-    }),
-    extend: {}
-  },
   variants: {
     backgroundColor: ["responsive", "hover", "focus", "active"],
     textColor: ["responsive", "hover", "focus", "active"],
     spinner: ["responsive"],
     transitionProperty: ["responsive"]
   },
-  plugins: [
-    //require("tailwindcss-dark-mode")(),
-    require("tailwindcss-spinner")(),
-    require("@tailwindcss/ui")
-    //require('tailwindcss-plugin-heroicons')({variants: ['responsive', 'hover']})
-  ]
+  plugins: []
 }
