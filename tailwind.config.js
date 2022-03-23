4532dcae8eea9809e5f59345c95d98b1cd5e9113
Perry Raskin <perryraskin@gmail.com>
2022-03-23 11:46:47 -0400

    Package updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
 module.exports = {
-  mode: 'jit',
-  darkMode: 'media',
-  purge: [
+  content: [
     "./pages/**/*.tsx",
     "./pages/**/*.jsx",
     "./components/**/*.tsx",
     "./components/**/*.jsx",
     "./public/index.html",
-    "./styles/tailwind.css"
+    "./styles/tailwind.css",
   ],
-  variants: {
-    backgroundColor: ["responsive", "hover", "focus", "active"],
-    textColor: ["responsive", "hover", "focus", "active"],
-    spinner: ["responsive"],
-    transitionProperty: ["responsive"]
-  },
-  plugins: []
+  plugins: [],
 }
